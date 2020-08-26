553d269a9ffa4bcecf4f8953c9b16ac69e411347
dioncdz <dioncadizjr@gmail.com>
2020-08-26 11:40:37 +0800

    Etch-A-Sketch trial one

---
+++ script.js
@@ -0,0 +1,43 @@
+// GET ELEMENTS
+let canvas = document.querySelector('.grid-container');
+let buttons = document.querySelectorAll('.buttons');
+let box = '<div class="grid"></div>';
+let hue = 0;
+console.dir(canvas);
+
+
+buttons.forEach(button => button.addEventListener('click', (e)=> {
+   let rows, column;
+   canvas.innerHTML = '';
+   
+   if (e.target.textContent === 'Set Grid') {
+      column = parseInt(prompt('Set number of columns (maximum: 64)', 16));
+      if (column > 64 || !column || column < 0) {
+         return -1;
+      } 
+      rows = parseInt(prompt('Set number of rows (maximum: 64)', 16)); 
+      if (rows > 64 || !rows || rows < 0) {
+         return -1;
+      } 
+      canvas.setAttribute('style', `grid-template-columns: repeat(${column}, 1fr)`)
+      for(let i = 0; i < column*rows; i++) {
+         canvas.innerHTML += box;
+      }
+   } else if (e.target.textContent === 'Clear Grid') {
+      canvas.innerHTML = '';
+   }
+}))
+
+canvas.addEventListener('mouseover', ()=>{
+   let boxes = document.querySelectorAll('.grid');
+   
+   boxes.forEach(box => box.addEventListener('mouseover', (e)=> {
+      e.target.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+      e.target.classList.remove('fade');
+   }))
+   boxes.forEach(box => box.addEventListener('mouseout', (e)=> {
+      // e.target.style.backgroundColor = `#273F3B`;
+      e.target.classList.add('fade');
+   }))
+   hue++;
+})
